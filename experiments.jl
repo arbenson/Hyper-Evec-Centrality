0b541f1d1d474d7547f8f7960897cb1700dbaa5e
Austin Benson <arbenson@gmail.com>
2018-07-21 10:51:41 -0400

    updates

--- experiments.jl
+++ experiments.jl
@@ -11,7 +11,7 @@ function collect_data(dataset::String, exact::Bool, kunif::Int)
     labels = read_node_labels(dataset)
     lcc_labels = labels[find(lcc)]
     get_top_labels(c::Vector{Float64}) =
-        lcc_labels[sortperm(c, rev=true)[1:10]]
+        lcc_labels[sortperm(c, rev=true)[1:20]]
     cec_c, cec_conv = CEC(Tcc)
     zec_c, zec_conv  = ZEC(Tcc)
     hec_c, hec_conv  = HEC(Tcc)
@@ -36,7 +36,7 @@ function collect_data(dataset::String, exact::Bool, kunif::Int)
     println("--------------\n")
 end
 
-function rank_corr(dataset::String, exact::Bool)
+function rank_corr(dataset::String, exact::Bool, start::Int64)
     close()
     exact_int = convert(Int64, exact)
     for k in [3, 4, 5]
@@ -45,42 +45,24 @@ function rank_corr(dataset::String, exact::Bool)
         sp = sortperm(cec_c)
         cec_c, hec_c, zec_c = cec_c[sp], hec_c[sp], zec_c[sp]
 
-        ran = 9:(length(cec_c) - 1)
+        start, finish = log10(start - 1), log10(length(cec_c) - 1
+        ran = [convert(Int64, round(v)) for v in logspace(start, finish), 500)]
         cec_hec = [corspearman(cec_c[(end-s):end], hec_c[(end-s):end]) for s in ran]
         cec_zec = [corspearman(cec_c[(end-s):end], zec_c[(end-s):end]) for s in ran]
-        hec_zec  = [corspearman(hec_c[(end-s):end],  zec_c[(end-s):end]) for s in ran]
+        hec_zec = [corspearman(hec_c[(end-s):end], zec_c[(end-s):end]) for s in ran]
         figure()
         xs = collect(ran) + 1
         semilogx(xs, cec_hec, linestyle="-",  lw=3, label="CEC/HEC")
         semilogx(xs, cec_zec, linestyle="-",  lw=1, label="CEC/ZEC")
-        semilogx(xs, hec_zec,  linestyle="--", lw=2, label="HEC/ZEC")
-        fsz = 18
+        semilogx(xs, hec_zec, linestyle="--", lw=2, label="HEC/ZEC")
+        fsz = 20
         ax = gca()
         ax[:tick_params](axis="both", labelsize=fsz-2, length=7, width=1.5)
         ax[:tick_params](axis="x",    which="minor", length=4, width=1)
-        legend(fontsize=fsz-2, frameon=false)
+        if k == 3; legend(fontsize=fsz-2, frameon=false); end
         title("$(dataset) ($(k)-uniform)", fontsize=fsz)
         xlabel("Number of top-ranked elements", fontsize=fsz)
         ylabel("Spearman's rank corr. coeff.", fontsize=fsz)
         savefig("output/$(dataset)-$(k)-rankcorr.eps", bbox_inches="tight")
     end
-end
-
-function tops(dataset::String, exact::Bool, kunif::Int64, numtop::Int64)
-    exact_int = convert(Int64, exact)
-    data = matread("output/$dataset-$(kunif)-$(exact_int).mat")
-    lcc_labels = data["lcc_labels"]
-    cec_c, hec_c, zec_c = data["cec_c"], data["hec_c"], data["zec_c"]
-
-    get_top_labels(c::Vector{Float64}) =
-        lcc_labels[sortperm(c, rev=true)[1:numtop]]
-
-    cec_labels = get_top_labels(cec_c)
-    zec_labels = get_top_labels(zec_c)
-    hec_labels = get_top_labels(hec_c)
-    println("k = $(kunif)")
-    for (ind, (x, y, z)) in enumerate(zip(cec_labels, zec_labels, hec_labels))
-        println("$ind: $x\t\t$y\t\t$z")
-    end
-    println("--------------\n")
 end
---
+++ paper_tables.jl
@@ -0,0 +1,53 @@
+include("HypergraphEvecCentrality.jl")
+using MAT
+
+function summary_statistics(dataset::String, exact::Bool, kunif::Int)
+    exact_int = convert(Int64, exact)
+    T = read_data_unweighted(dataset, kunif, exact)
+    Tcc, lcc = largest_component(T)
+    nnodes = Tcc.dimension
+    nnz = length(Tcc.I1)
+    println("$(nnodes) & $(nnz) &")
+end
+
+function top_ranked(dataset::String, exact::Bool, numtop::Int64, combined::Bool=false)
+    exact_int = convert(Int64, exact)
+
+    label_mat = Array{String}(numtop, 9)
+    col_ind = 1
+
+    for k in [3, 4, 5]
+        data = matread("output/$dataset-$(k)-$(exact_int).mat")
+        lcc_labels = data["lcc_labels"]
+        cec_c, hec_c, zec_c = data["cec_c"], data["hec_c"], data["zec_c"]
+
+        get_top_labels(c::Vector{Float64}) =
+            [string(s) for s in lcc_labels[sortperm(c, rev=true)[1:numtop]]]
+        
+        label_mat[:, col_ind] = get_top_labels(cec_c); col_ind += 1
+        label_mat[:, col_ind] = get_top_labels(zec_c); col_ind += 1
+        label_mat[:, col_ind] = get_top_labels(hec_c); col_ind += 1
+
+        local_label_mat = Array{String}(numtop, 3)
+        local_label_mat[:, 1] = get_top_labels(cec_c)
+        local_label_mat[:, 2] = get_top_labels(zec_c)
+        local_label_mat[:, 3] = get_top_labels(hec_c)
+
+        if !combined
+            for i in 1:numtop
+                str = join(collect(local_label_mat[i, :]), " & ")
+                str = "& $i & $(str) \\\\"
+                println(str)
+            end
+        end
+    end
+
+    if combined
+        for i in 1:numtop
+            strs = [split(s, "[")[1] for s in collect(label_mat[i, :])]
+            str = join(strs, " & ")
+            str = "$i & $(str) \\\\"
+            println(str)
+        end
+    end
+end
