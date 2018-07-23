b90d814901b8baca0a804e3bbea7eb7f579d1886
Austin Benson <arbenson@gmail.com>
2018-07-23 14:49:12 -0400

    testing code

--- test.jl
+++ test.jl
@@ -1,63 +1,118 @@
 include("HypergraphEvecCentrality.jl")
-
+using Base.Test
 using Combinatorics
 
-function main()
-    dim = 7
-    m = 3
-
-    hedges = collect(combinations(1:dim, m))
-    total = 2^length(hedges)
-    for test_hgraph in combinations(hedges, 4)
-    #for ind in shuffle(collect(0:(total-1)))
-        #keeps = [parse(Bool, b) for b in bin(ind, length(hedges))]
-        #test_hgraph = hedges[keeps]
-        #=
-        T = SymTensor4([h[1] for h in test_hgraph],
-                       [h[2] for h in test_hgraph],
-                       [h[3] for h in test_hgraph],
-                       [h[4] for h in test_hgraph],                                   
-                       ones(Float64, length(test_hgraph)),
-                       dim, m)
-        =#
-        T = SymTensor3([h[1] for h in test_hgraph],
-                       [h[2] for h in test_hgraph],
-                       [h[3] for h in test_hgraph],
-                       ones(Float64, length(test_hgraph)),
-                       dim, m)
-
-        # Skip cases that aren't connected
-        lcc = largest_component(T)[2]
-        if length(find(lcc)) < dim; continue; end
-        
-        # Get evec / eval
-        z, conv = Z_evec_dynsys(T)
-        if !conv; continue; end
-        z2 = z / norm(z, 2)
-        eval = mean(apply(T, z2) ./ z2)
-
-        # Check for stability
-        Ustar = qr((I - z2 * z2') * randn(dim, dim - 1), thin=true)[1]
-        C = Ustar' * ((m - 1) * reduce(T, z2) - eval * eye(dim)) * Ustar
-        U = triu(C, 1)
-        λ = real.(eig(U + U' + diagm(diag(C)))[1])
-
-        if maximum(λ) > 0 && minimum(λ) < 0; @show λ, test_hgraph, z; end
+function test3()
+    dim = 11
+    srand(1234)
+    
+    entries = collect(combinations(1:dim, 3))
+    vals = rand(length(entries))
+    T = SymTensor3([e[1] for e in entries],
+                   [e[2] for e in entries],
+                   [e[3] for e in entries],
+                   vals, dim)
+    entry_vals = Dict{NTuple{3,Int64},Float64}()
+    for (e, v) in zip(entries, vals)
+        entry_vals[NTuple{3, Int64}(e)] = v
     end
+
+    y = zeros(Float64, dim)
+    x = rand(dim)
+    for i in 1:dim, j in 1:dim, k in 1:dim
+        key = NTuple{3,Int64}(sort([i, j, k]))
+        if haskey(entry_vals, key)
+            Tijk = entry_vals[key]
+            y[i] += Tijk * x[j] * x[k]
+        end
+    end
+    @test y ≈ apply(T, x)
+
+    Y = zeros(Float64, dim, dim)
+    for i in 1:dim, j in 1:dim, k in 1:dim
+        key = NTuple{3,Int64}(sort([i, j, k]))
+        if haskey(entry_vals, key)
+            Tijk = entry_vals[key]
+            Y[i, j] += Tijk * x[k]
+        end
+    end
+    @test Y ≈ full(reduce(T, x))
 end
 
-function test2()
-    # (λ, test_hgraph, z) =
-    # ([-2.82842, -2.82842, -2.82842, -2.06111, -1.41421, 0.646902],
-    # Array{Int64,1}[[1, 2, 3], [1, 2, 4], [3, 5, 6], [5, 6, 7]],
-    # [0.158494, 0.158494, 0.183012, 0.091507, 0.158494, 0.158494, 0.091507])
-    dim = 7
-    m = 3
-    T = SymTensor3([1, 1, 3, 5],
-                   [2, 2, 5, 6],
-                   [3, 4, 6, 7],
-                   ones(Float64, 4),
-                   dim, m)
-    M = reduce(T, ones(Float64, dim))
-    return M, T
+function test4()
+    dim = 10
+    srand(1234)
+    
+    entries = collect(combinations(1:dim, 4))
+    vals = rand(length(entries))
+    T = SymTensor4([e[1] for e in entries],
+                   [e[2] for e in entries],
+                   [e[3] for e in entries],
+                   [e[4] for e in entries],                   
+                   vals, dim)
+    entry_vals = Dict{NTuple{4,Int64},Float64}()
+    for (e, v) in zip(entries, vals)
+        entry_vals[NTuple{4,Int64}(e)] = v
+    end
+
+    y = zeros(Float64, dim)
+    x = rand(dim)
+    for i in 1:dim, j in 1:dim, k in 1:dim, l in 1:dim
+        key = NTuple{4,Int64}(sort([i, j, k, l]))
+        if haskey(entry_vals, key)
+            Tijkl = entry_vals[key]
+            y[i] += Tijkl * x[j] * x[k] * x[l]
+        end
+    end
+    @test y ≈ apply(T, x)
+
+    Y = zeros(Float64, dim, dim)
+    for i in 1:dim, j in 1:dim, k in 1:dim, l in 1:dim
+        key = NTuple{4,Int64}(sort([i, j, k, l]))
+        if haskey(entry_vals, key)
+            Tijkl = entry_vals[key]
+            Y[i, j] += Tijkl * x[k] * x[l]
+        end
+    end
+    @test Y ≈ full(reduce(T, x))
 end
+
+function test5()
+    dim = 9
+    srand(1234)
+    
+    entries = collect(combinations(1:dim, 5))
+    vals = rand(length(entries))
+    T = SymTensor5([e[1] for e in entries],
+                   [e[2] for e in entries],
+                   [e[3] for e in entries],
+                   [e[4] for e in entries],
+                   [e[5] for e in entries],                   
+                   vals, dim)
+    entry_vals = Dict{NTuple{5,Int64},Float64}()
+    for (e, v) in zip(entries, vals)
+        entry_vals[NTuple{5,Int64}(e)] = v
+    end
+
+    y = zeros(Float64, dim)
+    x = rand(dim)
+    for i in 1:dim, j in 1:dim, k in 1:dim, l in 1:dim, m in 1:dim
+        key = NTuple{5,Int64}(sort([i, j, k, l, m]))
+        if haskey(entry_vals, key)
+            Tijklm = entry_vals[key]
+            y[i] += Tijklm * x[j] * x[k] * x[l] * x[m]
+        end
+    end
+    @test y ≈ apply(T, x)
+
+    Y = zeros(Float64, dim, dim)
+    for i in 1:dim, j in 1:dim, k in 1:dim, l in 1:dim, m in 1:dim
+        key = NTuple{5,Int64}(sort([i, j, k, l, m]))
+        if haskey(entry_vals, key)
+            Tijklm = entry_vals[key]
+            Y[i, j] += Tijklm * x[k] * x[l] * x[m]
+        end
+    end
+    @test Y ≈ full(reduce(T, x))
+end
+;
